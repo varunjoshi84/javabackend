052d00942da091dbee154d562f2c150905ca3c28
varunjoshi84 <varunpndey@outlook.com>
2025-06-14 13:00:30 +0530

    prepared statement added

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/K23SA";  // your database name
+        String url = "jdbc:mysql://localhost:3306/sport_sync";  // your database name
         String user = "root";
         String password = "";
 
@@ -10,14 +10,29 @@ public class Main {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(url, user, password);
 
-            Statement stmt = conn.createStatement();
-            System.out.println("Connected to database");
-            ResultSet rs = stmt.executeQuery("SELECT * FROM user");  // replace with your table
-
+//            Statement stmt = conn.createStatement();
+//            System.out.println("Connected to database");
+//            ResultSet rs = stmt.executeQuery("SELECT * FROM users");  // replace with your table
+//
+//            while (rs.next()) {
+//              System.out.println("username: " + rs.getString(1)); //
+//                int id = rs.getInt("id");
+//                String username = rs.getString("username");
+//                String email = rs.getString("email");
+//                String role = rs.getString("role");
+//                System.out.println(id + " " + username + " " + email + " " + role);
+//            }
+//            conn.close();
+            PreparedStatement ps = conn.prepareStatement("select * from users");
+            ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                System.out.println("Row: " + rs.getString(1)); // prints first column, change index or use rs.getInt() etc.
+//              System.out.println("username: " + rs.getString(1)); //
+                int id = rs.getInt("id");
+                String username = rs.getString("username");
+                String email = rs.getString("email");
+                String role = rs.getString("role");
+                System.out.println(id + " " + username + " " + email + " " + role);
             }
-
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
