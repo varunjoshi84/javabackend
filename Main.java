d64c8214bc0f8f752b161d3081a7cbd008264979
varunjoshi84 <varunpndey@outlook.com>
2025-06-14 09:36:36 +0530

    database connected successfully

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
-import java.net.StandardSocketOptions;
+import java.sql.*;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String url = "jdbc:mysql://localhost:3306/K23SA";  // your database name
+        String user = "root";
+        String password = "";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, user, password);
+
+            Statement stmt = conn.createStatement();
+            System.out.println("Connected to database");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user");  // replace with your table
+
+            while (rs.next()) {
+                System.out.println("Row: " + rs.getString(1)); // prints first column, change index or use rs.getInt() etc.
+            }
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
